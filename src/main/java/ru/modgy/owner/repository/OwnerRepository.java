6dac3238960b918d6cc4dcfb1641894f77b21a0d
RodionovRL <Rodionov.ru@yandex.ru>
2025-02-07 07:24:57 +0300

    Owner search lowercase

--- src/main/java/ru/modgy/owner/repository/OwnerRepository.java
+++ src/main/java/ru/modgy/owner/repository/OwnerRepository.java
@@ -11,13 +11,15 @@ import java.util.Optional;
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
     Optional<Owner> findByMainPhoneOrOptionalPhone(String mainPhoneNumber, String optionalPhoneNumber);
 
-    @Query(value = "select owner from Owner as owner where :direction = 'name'" +
-                   " and (owner.firstName like concat ('%', :searchLine, '%')" +
-                   " or owner.middleName like concat ('%', :searchLine, '%')" +
-                   " or owner.lastName like concat ('%', :searchLine, '%'))" +
-                   " or :direction ='phone'" +
-                   " and (owner.mainPhone like concat ('%', :searchLine, '%')" +
-                   " or owner.optionalPhone like concat ('%', :searchLine, '%'))")
+    @Query(value = """
+            select owner from Owner as owner where :direction = 'name'
+             and (lower(owner.firstName) like concat('%', lower(:searchLine) , '%')
+             or lower(owner.middleName) like concat ('%', lower(:searchLine), '%')
+             or lower(owner.lastName) like concat ('%', lower(:searchLine), '%'))
+             or :direction ='phone'
+             and (owner.mainPhone like concat ('%', :searchLine, '%')
+             or owner.optionalPhone like concat ('%', :searchLine, '%'))
+            """)
     List<Owner> searchOwner(
             @Param("searchLine") String searchLine,
             @Param("direction") String direction
